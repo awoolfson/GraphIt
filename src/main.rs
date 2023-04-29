e6eba324fc091a079382b6bb7c5a7827f1a49881
Auden Woolfson <auden.woolfson@gmail.com>
2023-04-28 21:02:25 -0400

    version 1 complete! still needs documentation, negatives implementation, trig functions

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 // each horizontal represents one print on the graph, so one y value
 use::std::env;
-use colored::Colorize;
+use std::io::*;
 mod parser;
-
+pub use parser::*;
 struct Horizontal {
     x_window: (i64, i64, usize),
     points: Vec<usize>,
@@ -31,36 +31,72 @@ impl Horizontal {
         assert!(index < s_bytes.len());
         assert!(s_bytes[index].is_ascii());
         assert!(newchar.is_ascii());
-        // we've made sure this is safe.
         s_bytes[index] = newchar as u8;
     }
 }
 
 fn main() {
-    env::set_var("RUST_BACKTRACE", "1");
-    //let realist = false; // if true the graph will be accurate to character spacing
-    // x_window and y_window are the ranges of the graph, and the size of the graph, will take in as arguments later
+    let debug = false;
+    if debug {
+        env::set_var("RUST_BACKTRACE", "1");
+    }
+
+    let mut x_size: i64 = 32;
+    let mut y_size: i64 = 32;
+
+    let args: Vec<String> = env::args().collect();
+    for (idx, a) in args.iter().enumerate() {
+        if a == "-xsize" {
+            x_size = args[idx + 1].parse::<i64>().unwrap();
+        } else if a == "-ysize" {
+            y_size = args[idx + 1].parse::<i64>().unwrap();
+        }
+    }
+
+    println!("f(x) = ...");
+    let mut input_string = String::new();
+    std::io::stdin().read_line(&mut input_string);
+    let clean_input = String::from(input_string.trim());
+    
+
+    let tokens = parser::parser::tokenize(clean_input).unwrap();
+
+    if debug { 
+        println!("\ninitial tokens\n");
+        for t in &tokens {
+            match t {
+                parser::parser::Token::Num(n) => println!("{}", n),
+                parser::parser::Token::Operator(o) => println!("{:?}", o),
+                parser::parser::Token::Var => println!("x"),
+            }
+        }
+    }
+
+    let postfix = parser::parser::infix_to_postfix(tokens);
+
+    if debug {
+        println!("postfix tokens\n");
+        for t in &postfix {
+            match t {
+                parser::parser::Token::Num(n) => println!("{}", n),
+                parser::parser::Token::Operator(o) => println!("{:?}", o),
+                parser::parser::Token::Var => println!("x"),
+            }
+        }
+    }
+
     let x_window;
     let y_window;
-    // if realist {
-    //     x_window = (-32, 32, 64); // lower, upper, size
-    //     y_window = (-32, 32, 64);
-    // } else {
-    //     x_window = (-32, 32, 64);
-    //     y_window = (-16, 16, 32);
-    // }
 
     //args
 
     x_window = (-32, 32, 64);
     y_window = (-16, 16, 32);
     let increment_parts = 1; // how many parts to divide each 1 by for iteration along x axis, 1 is the minimum
-    let x_size = 20; // scope of equation, not actual size on screen
-    let y_size = 4;
 
     let x_normalizer: f32 = x_window.2 as f32 / x_size as f32; // for conversion from real coords to math coords (/)
     let y_normalizer: f32 = y_window.2 as f32 / y_size as f32; // for conversion from math coords to real coords (*)
-    println!("x normalizer: {}, y_normalizer: {}", x_normalizer, y_normalizer);
+    if debug { println!("x normalizer: {}, y_normalizer: {}", x_normalizer, y_normalizer); }
 
     let mut lines = Vec::new();
     for _ in 0..y_window.2 {
@@ -72,61 +108,57 @@ fn main() {
     }
 
     for x_val in x_window.0..x_window.1 {
+        // consider removing the increment feature, maybe this is silly
         let normalized_x = x_val as f32 / x_normalizer;
         for i in 0..increment_parts {
             let incremented_x = normalized_x + (i as f32 / (x_size as f32)/increment_parts as f32);
-            let raw_height = math(incremented_x);
+            let raw_height = math_on_postfix(&postfix, incremented_x);
     
-            println!("x: {}, normalized_x: {}, raw height: {}", x_val, normalized_x, raw_height);
-    
-            // if !realist {
-            //     raw_height /= 2.0;
-            // }
+            if debug { println!("x: {}, normalized_x: {}, raw height: {}", x_val, normalized_x, raw_height); }
     
             let normalized_y = raw_height * y_normalizer;
     
             if normalized_y > y_window.0 as f32 && normalized_y < y_window.1 as f32 {
                 let horizontals_index = height_to_index(normalized_y, y_window);
-                println!("normalized y: {}, y index: {}\n", normalized_y, horizontals_index);
+                if debug { println!("normalized y: {}, y index: {}\n", normalized_y, horizontals_index); }
                 lines[horizontals_index as usize].points.push((x_val - x_window.0).try_into().unwrap());
             } else {
-                println!("y out of range\n");
+                if debug { println!("y out of range\n"); }
             }
         }
     }
 
     lines[y_window.2 as usize / 2].is0 = true;
-
-    /*
-    an issue with the following loops is that they can create "clusters", where there is a square of 4 points.
-    this happens because it cannot tell if the points are next to eachother and it will still clone, however the
-    problem of finding which point to clone is very difficult, so this should be fine for now.
-
-    also doesn't work with parabolo, sends a straight line out!
-
-    example of cluster:
-    x * x * x with size 50
-
-    This could potentially be fixed by actually making a more precise graph, incementing by 0.025 instead of 1 and then rounding
-     */
-
-    // for i in 0..lines.len() - 1 {
-    //     while !lines[i].points.is_empty() && lines[i + 1].points.is_empty() {
-    //         lines[i + 1].points = lines[i].points.clone();
-    //     }
-    // }
-    // for i in (0..lines.len()).rev() {
-    //     while lines[i].points.is_empty() && !lines[i + 1].points.is_empty() {
-    //         lines[i].points = lines[i + 1].points.clone();
-    //     }
-    // }
     lines.iter().for_each(|x| x.print());
 }
 
 // math is the function that is being graphed, will code parser later
-fn math(x: f32) ->  f32 {
-    let output = x.sin();
-    output
+// fn math(x: f32) ->  f32 {
+//     let output = x.sin();
+//     output
+// }
+
+fn math_on_postfix(postfix: &Vec<parser::parser::Token>, x: f32) -> f32 {
+    let mut stack: Vec<f32> = Vec::new();
+    for t in postfix {
+        match t {
+            parser::parser::Token::Num(n) => stack.push(*n),
+            parser::parser::Token::Var => stack.push(x),
+            parser::parser::Token::Operator(o) => {
+                let num1 = stack.pop().unwrap();
+                let num2 = stack.pop().unwrap();
+                match o {
+                    parser::parser::Operator::Exp(_) => stack.push(num2.powf(num1)),
+                    parser::parser::Operator::Multiply(_) => stack.push(num2 * num1),
+                    parser::parser::Operator::Divide(_) => stack.push(num2 / num1),
+                    parser::parser::Operator::Add(_) => stack.push(num2 + num1),
+                    parser::parser::Operator::Subtract(_) => stack.push(num2 - num1),
+                    _=> {},
+                }
+            }
+        }
+    }
+    stack[0]
 }
 
 // used to convert the height to the index of the horizontal
--- src/parser.rs
+++ src/parser.rs
@@ -3,30 +3,125 @@ pub mod parser {
     pub enum Token {
         Operator(Operator),
         Num(f32),
+        Var,
     }
 
+    #[derive(Debug)]
     pub enum Operator {
-        Exp,
-        Multiply,
-        Divide,
-        Add,
-        Subtract,
-        LeftParen,
-        RightParen,
+        Exp(char),
+        Multiply(char),
+        Divide(char),
+        Add(char),
+        Subtract(char),
+        LeftParen(char),
+        RightParen(char),
     }
 
     impl Operator {
         pub fn get_precedence_and_is_left(&self) -> (usize, bool) {
             match self {
-                Operator::Exp => (4, false),
-                Operator::Multiply => (3, true),
-                Operator::Divide => (3, true),
-                Operator::Add => (2, true),
-                Operator::Subtract => (2, true),
-                Operator::LeftParen => (9, true),
-                Operator::RightParen => (0, true),
+                Operator::Exp(_) => (4, false),
+                Operator::Multiply(_) => (3, true),
+                Operator::Divide(_) => (3, true),
+                Operator::Add(_) => (2, true),
+                Operator::Subtract(_) => (2, true),
+                Operator::LeftParen(_) => (9, true),
+                Operator::RightParen(_) => (0, true),
             }
         }
+    }
+
+    pub fn tokenize(input: String) -> Result<Vec<Token>, &'static str> {
+        let mut output: Vec<Token> = Vec::new();
+        let mut cur_num: String = String::new();
+        for c in input.chars() {
+            match c {
+                '0'..='9' => {
+                    cur_num.push(c);
+                },
+                '.' => cur_num.push(c),
+                '+' => {
+                    if !cur_num.is_empty() {
+                        let num = cur_num.parse::<f32>().unwrap();
+                        output.push(Token::Num(num));
+                        cur_num = String::new();
+                    }
+                    output.push(Token::Operator(Operator::Add('+')));
+                }
+                '-' => {
+                    if !cur_num.is_empty() {
+                        let num = cur_num.parse::<f32>().unwrap();
+                        output.push(Token::Num(num));
+                        cur_num = String::new();
+                    }
+                    output.push(Token::Operator(Operator::Subtract('-')));
+                }
+                '*' => {
+                    if !cur_num.is_empty() {
+                        let num = cur_num.parse::<f32>().unwrap();
+                        output.push(Token::Num(num));
+                        cur_num = String::new();
+                    }
+                    output.push(Token::Operator(Operator::Multiply('*')));
+                }
+                '/' => {
+                    if !cur_num.is_empty() {
+                        let num = cur_num.parse::<f32>().unwrap();
+                        output.push(Token::Num(num));
+                        cur_num = String::new();
+                    }
+                    output.push(Token::Operator(Operator::Divide('/')));
+                }
+                '^' => {
+                    if !cur_num.is_empty() {
+                        let num = cur_num.parse::<f32>().unwrap();
+                        output.push(Token::Num(num));
+                        cur_num = String::new();
+                    }
+                    output.push(Token::Operator(Operator::Exp('^')));
+                }
+                '(' => {
+                    if !cur_num.is_empty() {
+                        let num = cur_num.parse::<f32>().unwrap();
+                        output.push(Token::Num(num));
+                        cur_num = String::new();
+                    }
+                    output.push(Token::Operator(Operator::LeftParen('(')));
+                }
+                ')' => {
+                    if !cur_num.is_empty() {
+                        let num = cur_num.parse::<f32>().unwrap();
+                        output.push(Token::Num(num));
+                        cur_num = String::new();
+                    }
+                    output.push(Token::Operator(Operator::RightParen(')')));
+                }
+                'x' => {
+                    if !cur_num.is_empty() {
+                        let num = cur_num.parse::<f32>().unwrap();
+                        output.push(Token::Num(num));
+                        cur_num = String::new();
+                    }
+                    output.push(Token::Var);
+                }
+                ' ' => {}
+                _ => {
+                    println!("Invalid character found: {}", c);
+                    return Err("Invalid character");
+                }
+            }
+        }
+        if !cur_num.is_empty() {
+            output.push(Token::Num(cur_num.parse::<f32>().unwrap()));
+        }
+        // for t in &output {
+        //     match t {
+        //         Token::Num(n) => println!("{}", n),
+        //         Token::Operator(o) => println!("{:?}", o),
+        //         Token::Var => println!("x"),
+        //     }
+        // }
+        Ok(output)
     }
 
     pub fn infix_to_postfix(infix: Vec<Token>) -> Vec<Token>{
@@ -34,7 +129,8 @@ pub mod parser {
         let mut stack: Vec<Operator> = Vec::new();
         for t in infix {
             match t {
-                Token::Num(n) => output_queue.push_back(t),
+                Token::Num(_) => output_queue.push_back(t),
+                Token::Var => output_queue.push_back(t),
                 Token::Operator(o) => {
                     while !stack.is_empty() { 
                         let cur_operator = &o;
@@ -44,9 +140,9 @@ pub mod parser {
                         if (cur_prec_and_left.1 && cur_prec_and_left.0 <= tos_prec_and_left.0)
                         || (!cur_prec_and_left.1 && cur_prec_and_left.0 < tos_prec_and_left.0) {
                             match cur_operator {
-                                Operator::RightParen => {
+                                Operator::RightParen(_) => {
                                     match top_of_stack {
-                                        Operator::LeftParen => {
+                                        Operator::LeftParen(_) => {
                                             stack.pop();
                                             break;
                                         },
@@ -58,7 +154,7 @@ pub mod parser {
                                 },
                                 _=> {
                                     match top_of_stack {
-                                        Operator::LeftParen => break,
+                                        Operator::LeftParen(_) => break,
                                         _=> {
                                             let to_push = Token::Operator(stack.pop().unwrap());
                                             output_queue.push_back(to_push);
@@ -72,7 +168,7 @@ pub mod parser {
                     } 
                     let cur_operator = o;
                     match cur_operator {
-                        Operator::RightParen => {},
+                        Operator::RightParen(_) => {},
                         _=> {
                             stack.push(cur_operator);
                         }
